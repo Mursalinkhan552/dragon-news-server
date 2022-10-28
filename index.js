ed5dce9ac5ce21a20381023a63fa87bbe3ffe225
unknown <mursalinkhan552@gmail.com>
2022-10-28 09:32:29 -0700

    Something added

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 
 app.get('/', (req, res) => {
@@ -17,6 +18,28 @@ app.get('/news-categories', (req, res) => {
     res.send(categories);
 })
 
-app.listen(port, () =>{
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news);
+    }
+    else {
+        const categoryNews = news.filter(n => n.category_id === id);
+        res.send(categoryNews);
+    }
+
+})
+
+app.get('/news', (req, res) =>{
+    res.send(news);
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
+})
+
+app.listen(port, () => {
     console.log('Dragon News Server Running on Port', port);
 })
